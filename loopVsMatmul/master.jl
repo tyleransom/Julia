08d7b64a5d50e9428779894432567cf3f915fc59
Tyler Ransom <tyleransom@gmail.com>
2015-09-14 16:23:05 -0400

    Added additional timing to test compiler warm-up

--- loopVsMatmul/master.jl
+++ loopVsMatmul/master.jl
@@ -7,4 +7,6 @@ include("loopmul.jl")
 
 @time X,beta   = datagen(convert(Int64,1e5))
 @time Ymatmul  = matmul(X,beta)
+@time Ymatmul  = matmul(X,beta) #to test warm-up effect
 @time Yloopmul = loopmul(X,beta)
+@time Yloopmul = loopmul(X,beta) #to test warm-up effect
